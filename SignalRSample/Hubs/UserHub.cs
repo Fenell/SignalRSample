c031256461e171736ac21c47e412e9067698d47b
Tuấn Đạt <datmtph29475@fpt.edu.vn>
2023-09-03 16:00:06 +0700

    Initial commit

---
+++ SignalRSample/Hubs/UserHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs;
+
+public class UserHub:Hub
+{
+    private static int TotalViews { get; set; }  
+    
+    public async Task NewWindowLoaded()
+    {
+        TotalViews++;
+        await Clients.All.SendAsync("updateTotalViews", TotalViews);
+    }
+    
+}
---
+++ SignalRSample/wwwroot/js/userCount.js
@@ -0,0 +1,23 @@
+﻿//Create connection
+var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//Connect to methods that hub invokes aka receive notification from hub
+ connection.on("updateTotalViews", (value)=>{
+     var newCountSpan = document.getElementById("totalViewsCounter");
+     newCountSpan.innerText = value.toString();
+ });
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient(){
+     connection.send("NewWindowLoaded");
+}
+
+// start connection
+function successful(){
+     console.log("Connect to User hub successful");
+     newWindowLoadedOnClient();
+}
+function rejected(){
+     
+}
+connection.start().then(successful, rejected);
